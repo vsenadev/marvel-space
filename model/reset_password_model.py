08be9972043c5c2f3c16ce48b6ed99a760e6414e
Vinícius Sena <senavinicius01@gmail.com>
2023-08-22 02:19:39 -0300

    route for generate code, authenticate user and search in database for history generate code

--- model/reset_password_model.py
+++ model/reset_password_model.py
@@ -1,7 +1,6 @@
 class ResetPassword:
-    def __init__(self, email, password, code, date, validity):
+    def __init__(self, email, code, date, validity):
         self.email = email
-        self.password = password
         self.code = code
         self.date = date
         self.validity = validity
--- utils/reset_password_utils.py
+++ utils/reset_password_utils.py
@@ -1,7 +1,21 @@
 from utils.mail_utils import MailUtils
+import random
+
 
 class ResetPasswordUtils:
     mail_utils = MailUtils()
-    def validate_attempt(self):
-        self.mail_utils.send_token_email()
-        # return self.collection.find({email: email})
+
+    def validate_attempt(self, email):
+        code = self.generate_code()
+        return self.mail_utils.send_token_email(email, code)
+
+    def generate_code(self):
+        random_numbers = []
+        for _ in range(5):
+            number = random.randint(1,9)
+            random_numbers.append(number)
+
+        number_str = ' '.join(map(str, random_numbers))
+
+        return number_str
+
