4b39e85c17abcca2e7c104107672b9008cdf24d4
Vinícius Sena <senavinicius01@gmail.com>
2023-09-26 23:56:13 -0300

    generate html with data

--- documentation/static/main.js
+++ documentation/static/main.js
@@ -1,12 +1,12 @@
 const data = {
     login: {
         createUser: {
-            description: "Create a user.",
+            description: "This API route allows the creation of a new user in the system. By sending an HTTP POST request to this endpoint, the data of the new user is processed and stored in the system's database. The newly created user will receive a unique identifier, typically a unique numerical ID, and will be added to the list of registered users in the system.",
             params: [
-                { name: "string" },
-                { login: "string" },
-                { email: "string" },
-                { password: "string" },
+                { name: "name", type: "string" },
+                { name: "login", type: "string" },
+                { name: "email", type: "string" },
+                { name: "password", type: "string" },
             ],
             url: "http://192.168.100.71:3050/api/v1/login",
             method: "POST",
@@ -21,10 +21,31 @@ const data = {
 };
 
 const route = document.getElementById("url");
+const title = document.querySelector(".container__content_main_title");
+const description = document.querySelector(".container__content_main_section_description")
+const container = document.querySelector(".container__content_main_section_div")
+
 if (route) {
     route.addEventListener("click", (event) => {
         if(event.target.textContent === "Create User"){
-            console.log(data.login.createUser)
+            title.textContent = "Create User"
+            description.textContent = data.login.createUser.description
+            container.innerHTML = ''
+            let routeParamsString = ''
+
+            data.login.createUser.params.map((param) => {
+                routeParamsString += `<span>${param.name} <span>${param.type}</span></span>`
+            })
+
+            const routeData = `
+                <section>
+                    <div>
+                        ${routeParamsString}
+                    </div>
+                </section>
+            `
+
+            container.innerHTML = routeData
         }
     });
 } else {
