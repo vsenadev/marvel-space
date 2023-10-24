e40be0c43bc7b2a9121157e5e07ec37563bed955
Vinícius Sena <senavinicius01@gmail.com>
2023-10-24 01:01:16 -0300

    battle game finished

--- utils/character_utils.py
+++ utils/character_utils.py
@@ -1,3 +1,23 @@
 class CharacterUtils:
     def battle(self, player1, player2):
-        pass
+        result = []
+        attributes = ['affiliations', 'abilities_and_powers', 'strength', 'speed', 'durability', 'agility',
+                      'combat_experience', 'recovery', 'intelligence', 'equipment']
+
+        for attr in attributes:
+            p1_value = int(player1[attr])
+            p2_value = int(player2[attr])
+
+            if p1_value > p2_value:
+                result.append(player1['name'])
+            elif p2_value > p1_value:
+                result.append(player2['name'])
+
+        player1_Occurrences = result.count(player1['name'])
+        player2_Occurrences = result.count(player2['name'])
+
+        if player1_Occurrences > player2_Occurrences:
+            return "{0} Win".format(player1['name'])
+        else:
+            return "{0} Win".format(player2['name'])
+
